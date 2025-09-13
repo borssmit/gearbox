b75a7be06a765792fe762b02be591bc5cb93e8b9
borssmit <borssmitt@ukr.net>
2025-09-13 12:59:04 +0300

    Add local_settings

--- gearbox/settings.py
+++ gearbox/settings.py
@@ -124,3 +124,8 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like no local file. You must be on production")
